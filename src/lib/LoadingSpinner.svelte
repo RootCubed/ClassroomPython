62e939a2b734396d82c2435e8ea520d6c5f7c2b3
RootCubed <liam@rootcubed.dev>
2024-06-01 17:54:21 +0200

    feat: spinner for toolbar buttons

---
+++ src/lib/LoadingSpinner.svelte
@@ -0,0 +1,10 @@
+<svg
+    aria-hidden="true"
+    class="inline h-6 w-6 animate-spin fill-gray-100 text-gray-400 dark:fill-gray-300 dark:text-gray-500"
+    viewBox="-5 -5 10 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path d="m-5 0a1 1 0 0010 0 1 1 0 00-10 0m1 0a1 1 0 018 0 1 1 0 01-8 0" fill="currentColor" />
+    <path d="m-5 0a5 5 0 015-5c1 0 1 1 0 1a4 4 0 00-4 4c0 1-1 1-1 0" fill="currentFill" />
+</svg>
--- src/lib/Toolbar.svelte
+++ src/lib/Toolbar.svelte
@@ -2,9 +2,32 @@
     import { Button } from "$lib/components/ui/button";
     import * as Tooltip from "$lib/components/ui/tooltip";
     import { Play, Rocket, Save } from "lucide-svelte";
+    import LoadingSpinner from "./LoadingSpinner.svelte";
+    import { cn } from "./utils";
 
     export let onExecute = () => {};
-    export let onSubmit = () => {};
+    export let onSave = () => {};
+    export let onSubmit = async () => {};
+
+    let isSaving = false;
+    let isSubmitting = false;
+
+    // So that the spinner is briefly visible even if the submission happens instantly
+    async function artificialDelay(func: () => any, amount = 200) {
+        await Promise.all([func(), new Promise((resolve) => setTimeout(resolve, amount))]);
+    }
+
+    async function handleSave() {
+        isSaving = true;
+        await artificialDelay(onSave);
+        isSaving = false;
+    }
+
+    async function handleSubmit() {
+        isSubmitting = true;
+        await artificialDelay(onSubmit);
+        isSubmitting = false;
+    }
 </script>
 
 <div class="flex flex-row items-center gap-2 px-2">
@@ -21,18 +44,40 @@
     </Tooltip.Root>
     <Tooltip.Root>
         <Tooltip.Trigger>
-            <Button variant="destructive"><Save /></Button></Tooltip.Trigger
+            <Button
+                variant="secondary"
+                on:click={handleSave}
+                class={cn(
+                    "bg-rose-600 text-white hover:bg-rose-700 dark:bg-rose-800 dark:hover:bg-rose-900",
+                    { "opacity-50": isSaving }
+                )}
+                disabled={isSaving}
+            >
+                {#if isSaving}
+                    <LoadingSpinner />
+                {:else}
+                    <Save />
+                {/if}
+            </Button></Tooltip.Trigger
         >
         <Tooltip.Content>Code speichern</Tooltip.Content>
     </Tooltip.Root>
     <Tooltip.Root>
-        <Tooltip.Trigger>
+        <Tooltip.Trigger asChild>
             <Button
                 variant="secondary"
-                on:click={onSubmit}
-                class="bg-blue-600 text-white
-                hover:bg-blue-700 dark:bg-blue-800 dark:hover:bg-blue-900"><Rocket /></Button
-            ></Tooltip.Trigger
+                on:click={handleSubmit}
+                class={cn(
+                    "bg-blue-600 text-white hover:bg-blue-700 dark:bg-blue-800 dark:hover:bg-blue-900"
+                )}
+                disabled={isSubmitting}
+            >
+                {#if isSubmitting}
+                    <LoadingSpinner />
+                {:else}
+                    <Rocket />
+                {/if}
+            </Button></Tooltip.Trigger
         >
         <Tooltip.Content>Einreichen</Tooltip.Content>
     </Tooltip.Root>
