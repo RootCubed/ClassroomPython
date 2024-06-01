d9e72e2b26d0813e094622f24fc74c25ef497c09
RootCubed <liam@rootcubed.dev>
2024-06-01 15:21:13 +0200

    feat: show user menu even if no exercise is selected

--- src/routes/exercise/+layout.svelte
+++ src/routes/exercise/+layout.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
-    import SidebarLayout from "$lib/SidebarLayout.svelte";
     import type { PageData } from "./$types";
+    import SidebarLayout from "$lib/SidebarLayout.svelte";
+    import { Separator } from "$lib/components/ui/separator";
+    import UserMenu from "$lib/UserMenu.svelte";
+    import { loadedExercise } from "$lib/page-state";
+    import { page } from "$app/stores";
 
     export let data: PageData;
 </script>
 
 <SidebarLayout exercises={data.exercises}>
-    <slot></slot>
+    <div class="ml-16 flex content-around items-center p-2 md:ml-0">
+        <span class="text-lg">
+            {#if $page.params.id && $loadedExercise}
+                {$loadedExercise.title}
+            {:else}
+                Home
+            {/if}
+        </span>
+        <UserMenu />
+    </div>
+    <Separator class="mb-2" />
+    <div class="flex h-full flex-col">
+        <slot></slot>
+    </div>
 </SidebarLayout>
