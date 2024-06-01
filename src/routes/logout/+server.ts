999eb071bd1fbfb85b2983e09aba5e4299f5a098
RootCubed <liam@rootcubed.dev>
2024-06-01 15:19:35 +0200

    feat: logout endpoint

---
+++ src/routes/logout/+server.ts
@@ -0,0 +1,13 @@
+import type { RequestHandler } from "./$types";
+import * as db from "$lib/server/db";
+import { redirect } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ cookies }) => {
+    const sessionToken = cookies.get("session");
+    if (sessionToken) {
+        await db.invalidateSession(sessionToken);
+        cookies.delete("session", { path: "/" });
+    }
+
+    redirect(302, "/login");
+};
