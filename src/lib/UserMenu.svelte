791b4d419ed329984f422cd898b865ecc53d9c2f
RootCubed <liam@rootcubed.dev>
2024-06-01 15:20:54 +0200

    feat: link to logout in user menu

--- src/lib/UserMenu.svelte
+++ src/lib/UserMenu.svelte
@@ -3,6 +3,7 @@
     import { Button } from "$lib/components/ui/button";
     import { ChevronDown, User } from "lucide-svelte";
     import { user } from "$lib/page-state";
+    import { goto } from "$app/navigation";
 </script>
 
 <div class="ml-auto flex items-center">
@@ -11,14 +12,14 @@
             <Button builders={[builder]} variant="secondary" class="flex gap-2 px-2">
                 <User size={16} />
                 {$user.fullName}
-                <ChevronDown class="text-secondary-foreground h-4 w-4" />
+                <ChevronDown class="h-4 w-4 text-secondary-foreground" />
             </Button>
         </DropdownMenu.Trigger>
         <DropdownMenu.Content class="w-40">
             <DropdownMenu.Label class="font-normal">
                 <div class="flex flex-col space-y-1">
                     <p class="text-sm font-medium leading-none">{$user.fullName}</p>
-                    <p class="text-muted-foreground text-xs leading-none">{$user.name}</p>
+                    <p class="text-xs leading-none text-muted-foreground">{$user.name}</p>
                 </div>
             </DropdownMenu.Label>
             <DropdownMenu.Separator />
@@ -27,7 +28,7 @@
                 <DropdownMenu.Item>Einstellungen</DropdownMenu.Item>
             </DropdownMenu.Group>
             <DropdownMenu.Separator />
-            <DropdownMenu.Item>Ausloggen</DropdownMenu.Item>
+            <DropdownMenu.Item on:click={() => goto("/logout")}>Ausloggen</DropdownMenu.Item>
         </DropdownMenu.Content>
     </DropdownMenu.Root>
 </div>
