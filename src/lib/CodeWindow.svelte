e6b8e88bd7ad56807937dcf8f20596b0efb98c61
RootCubed <liam@rootcubed.dev>
2024-06-01 17:54:45 +0200

    feat: confirmation window for unsaved changes

--- src/lib/CodeWindow.svelte
+++ src/lib/CodeWindow.svelte
@@ -2,17 +2,20 @@
     import * as pyodide from "pyodide";
 
     import * as Resizable from "$lib/components/ui/resizable";
+    import * as AlertDialog from "$lib/components/ui/alert-dialog";
 
     import AceEditor from "./AceEditor.svelte";
     import Console from "./Console.svelte";
     import Toolbar from "./Toolbar.svelte";
 
     import { py } from "./page-state";
+    import { beforeNavigate, goto } from "$app/navigation";
 
     export let initialCode: string;
     export let submissionID: string | null = null;
 
-    let userCode = "";
+    let userCode = initialCode;
+    let lastSavedCode = userCode;
 
     let stdout = "";
 
@@ -45,22 +48,65 @@
         });
         if (resp.ok) {
             console.log("Submitted code");
+            lastSavedCode = userCode;
         } else {
             console.error("Failed to submit code");
         }
+    }
+
+    async function saveCode() {
+        lastSavedCode = userCode;
     }
 
     let editor: AceEditor | undefined;
 
     $: if (editor) {
         editor.setCode(initialCode);
+        lastSavedCode = initialCode;
     }
+
+    let leaveConfirmWindow = {
+        open: false,
+        destination: ""
+    };
+
+    function forceNavigate() {
+        lastSavedCode = userCode;
+        goto(leaveConfirmWindow.destination);
+    }
+
+    beforeNavigate(async (navigation) => {
+        if (userCode !== lastSavedCode) {
+            if (!navigation.willUnload) {
+                leaveConfirmWindow.open = true;
+                leaveConfirmWindow.destination = navigation.to?.url.toString() ?? "#";
+            }
+            navigation.cancel();
+        }
+    });
 </script>
+
+<AlertDialog.Root bind:open={leaveConfirmWindow.open}>
+    <AlertDialog.Content>
+        <AlertDialog.Header>
+            <AlertDialog.Title>Ungespeicherte Änderungen</AlertDialog.Title>
+            <AlertDialog.Description>
+                Du hast noch ungespeicherte Änderungen. Möchtest du wirklich fortfahren?
+            </AlertDialog.Description>
+        </AlertDialog.Header>
+        <AlertDialog.Footer>
+            <AlertDialog.Cancel>Abbrechen</AlertDialog.Cancel>
+            <AlertDialog.Action on:click={forceNavigate}
+                >Ohne Speichern fortfahren</AlertDialog.Action
+            >
+        </AlertDialog.Footer>
+    </AlertDialog.Content>
+</AlertDialog.Root>
 
 <Resizable.PaneGroup direction="vertical">
     <Resizable.Pane>
         <div class="flex h-full flex-col gap-2">
-            <Toolbar onExecute={runCode} onSubmit={submitCode} />
+            <Toolbar onExecute={runCode} onSave={saveCode} onSubmit={submitCode} />
             <AceEditor bind:value={userCode} bind:this={editor} />
         </div>
     </Resizable.Pane>
