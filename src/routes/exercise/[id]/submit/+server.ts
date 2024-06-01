b77464d1de63473d490e3c5fbc1af90d0df02b9e
RootCubed <liam@rootcubed.dev>
2024-06-01 15:19:57 +0200

    feat: submit as correct user

--- src/routes/exercise/[id]/submit/+server.ts
+++ src/routes/exercise/[id]/submit/+server.ts
@@ -1,16 +1,18 @@
 import type { RequestHandler } from "./$types";
 import * as db from "$lib/server/db";
 
-export const POST: RequestHandler = async ({ request, params }) => {
-    console.log(await db.getUsers());
-    // TODO: check if exercise exists
+export const POST: RequestHandler = async ({ cookies, request, params }) => {
+    const sessionToken = cookies.get("session");
+    if (!sessionToken) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+    const user = await db.checkSession(sessionToken);
+    if (!user) {
+        return new Response("Unauthorized", { status: 401 });
+    }
     try {
         const code = await request.text();
-        await db.addSubmission(
-            params.id,
-            "578dc6cc-18cc-431b-8e49-19808884f9d1", // TODO: get user id from session
-            code
-        );
+        await db.addSubmission(params.id, user.id, code);
     } catch (error) {
         console.error(error);
         return new Response("Internal Server Error", { status: 500 });
