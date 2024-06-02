38bca53da1809cff42f82ed746e89fa5c6ed16b3
RootCubed <liam@rootcubed.dev>
2024-06-02 21:43:16 +0200

    fix: error page in German

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="flex h-full flex-col items-center justify-center gap-2">
-    <span class="text-2xl">An error occured.</span>
+    <span class="text-2xl">Ein Fehler ist aufgetreten:</span>
     <span>{$page.error?.message}</span>
 </div>
