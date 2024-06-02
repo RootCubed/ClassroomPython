ad8f3e8967ff69ebefd63c854f2f954f9a5905e5
RootCubed <liam@rootcubed.dev>
2024-06-02 21:43:33 +0200

    fix: disallow regular user access to submission view

--- src/routes/exercise/[id]/admin/+page.server.ts
+++ src/routes/exercise/[id]/admin/+page.server.ts
@@ -1,7 +1,11 @@
 import * as db from "$lib/server/db";
-import type { Load } from "@sveltejs/kit";
+import { error, type Load } from "@sveltejs/kit";
 
-export const load: Load = async ({ params }) => {
+export const load: Load = async ({ params, parent }) => {
+    const { user } = await parent();
+    if (!user.isAdmin) {
+        error(403, "Zugriff nicht erlaubt.");
+    }
     const exercise = await db.adminGetExercise(params.id!);
     return {
         exercise
