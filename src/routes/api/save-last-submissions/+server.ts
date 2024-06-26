27de60526f7b82b92f313084c1a551f37c182a5a
RootCubed <liam@rootcubed.dev>
2024-06-26 08:42:42 +0200

    refactor: remove save-last-submissions endpoint (Temporary feature no longer needed)

--- src/routes/api/save-last-submissions/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from "./$types";
-import * as db from "$lib/server/db";
-
-export const GET: RequestHandler = async () => {
-    await db.saveLastSubmissions();
-
-    return new Response("OK", { status: 200 });
-};
