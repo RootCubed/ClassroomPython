f171c9fc11ebeb5017d6e2aeef1a625744345375
RootCubed <liam@rootcubed.dev>
2024-06-27 18:06:15 +0200

    fix: reset entire database with reset database button

--- src/routes/api/reset-database/+server.ts
+++ src/routes/api/reset-database/+server.ts
@@ -4,6 +4,7 @@ import * as db from "$lib/server/db";
 export const POST: RequestHandler = async () => {
     try {
         await db.resetAllExceptUsers();
+        await db.resetUsers();
     } catch (error) {
         console.error(error);
         return new Response("Internal Server Error", { status: 500 });
