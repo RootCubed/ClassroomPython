6f67d9405c871305eba154ee02dd21f2aaa8b6c0
RootCubed <liam@rootcubed.dev>
2024-12-06 16:33:14 +0100

    feat: stick console output to bottom and handle scroll better

--- src/lib/Console.svelte
+++ src/lib/Console.svelte
@@ -4,19 +4,29 @@
     import * as m from "$lib/paraglide/messages";
 
     let { consoleOutput = [] }: { consoleOutput: ConsoleOutput[] } = $props();
+
+    let scrollContainer: HTMLElement | null = null;
+
+    $effect(() => {
+        if (consoleOutput.length && scrollContainer != null) {
+            scrollContainer.scrollTop = scrollContainer.scrollHeight;
+        }
+    });
 </script>
 
 <div class="flex h-full flex-col bg-zinc-300 text-gray-900 dark:bg-zinc-900 dark:text-gray-300">
-    <span class="block w-full bg-zinc-200 p-2 dark:bg-zinc-800">{m.code_console()}</span>
-    <div class="h-full overflow-auto p-2">
-        {#each consoleOutput as chunk}
-            {#if chunk.type === "stdout"}
-                <pre>{chunk.text}</pre>
-            {:else if chunk.type === "stderr"}
-                <pre class="text-red-500">{chunk.text}</pre>
-            {:else if chunk.type === "extra"}
-                <pre class="text-green-500">{chunk.text}</pre>
-            {/if}
-        {/each}
+    <span class="w-full bg-zinc-200 p-2 dark:bg-zinc-800">{m.code_console()}</span>
+    <div class="relative h-full overflow-auto" bind:this={scrollContainer}>
+        <div class="absolute left-0 top-0 p-2">
+            {#each consoleOutput as chunk}
+                {#if chunk.type === "stdout"}
+                    <pre>{chunk.text}</pre>
+                {:else if chunk.type === "stderr"}
+                    <pre class="text-red-500">{chunk.text}</pre>
+                {:else if chunk.type === "extra"}
+                    <pre class="text-green-500">{chunk.text}</pre>
+                {/if}
+            {/each}
+        </div>
     </div>
 </div>
